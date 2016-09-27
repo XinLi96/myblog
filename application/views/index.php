4c41d09d015c33279d7fcb83853b4dc435e53e6f
XinLi96 <291750956@qq.com>
2016-09-27 22:32:46 +0800

    添加文件夹

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,21 @@
-<?php
 /**
  * Created by PhpStorm.
  * User: lixin
  * Date: 2016/9/27
  * Time: 22:00
  */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>首页</title>
+    <base href="">
+</head>
+<body>
+    <p>Hello word!</p>
+
+</body>
+</html>
